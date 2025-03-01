f8793f4b0a53125d7496871f3dcbb3a63020b69f
Sata Camden <sq@trashboat.org>
2025-02-28 19:08:57 -0500

    Routine Maintenance

--- internals/startup.sh
+++ internals/startup.sh
@@ -2,12 +2,11 @@
 # Determine the directory where this script resides
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Run the terminal scripts
-"$BASEDIR/startup_terminal.sh" &
-
-# Run the screen scripts
-"$BASEDIR/startup_screen.sh" &
-
-# Run the background scripts
+# Run background scripts
 "$BASEDIR/startup_background.sh" &
 
+# Run screen scripts
+"$BASEDIR/startup_screen.sh" &
+
+# Run terminal scripts
+"$BASEDIR/startup_terminal.sh" &
